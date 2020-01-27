3b438792ed817584ed8172a4808842674d85f8e7
Humberto <beto984@gmail.com>
2020-01-27 10:00:34 -0500

    Ajout du menu principal et du MODE_QUITTER

---
+++ INF111-04_H20_TP1/constApplications.java
@@ -0,0 +1,16 @@
+/**
+ * Classe contenant toutes les constantes utilisees
+ *
+ * @author Kenza Zniber et Humberto Villarino
+ * Date: 24 janvier 2020
+ */
+
+ public class constApplications{
+    // Constantes pour le menu
+   public final static int MODE_MENU_PRINCIPAL = 0;
+   public final static int MODE_MENU_CHOIX_NOTE = 1;
+   public final static int MODE_MENU_CHOIX_GAMME = 2;
+   public final static int MODE_QUITTER = 3; 
+   public final static String STRING_CHOIX = "Entrez votre choix ";
+ }
+ 
---
+++ INF111-04_H20_TP1/programmePrincipal.java
@@ -0,0 +1,51 @@
+import javafx.beans.value.WeakChangeListener;
+
+/**
+ * Classe qui contient le programme principal 
+ *
+ * @author Kenza Zniber et Humberto Villarino
+ * Date: 24 janvier 2020
+ */
+
+ public class programmePrincipal{
+    
+    static boolean fin=false; 
+    static boolean[][] manche = new boolean[6][25];
+    static int mode = constApplications.MODE_MENU_PRINCIPAL;
+
+    public static void initialiseMancheFaux(boolean manche[][]){
+        for(int i=0;i<6;i++){
+            for (int j =0; j<25; j++){
+                manche[i][j]=false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        do {
+        
+
+            switch (mode){
+                case constApplications.MODE_MENU_PRINCIPAL:
+                    initialiseMancheFaux(manche);
+                    System.out.println("Utilitaire pour guitare");
+                    System.out.println("---------------------------------------------");
+                    System.out.println("1 : Afficher une note sur le manche"); 
+                    System.out.println("2 : Afficher une gamme sur le manche");
+                    System.out.println("3 : Quitter");
+                    System.out.println("---------------------------------------------");
+                    mode=UtilitaireValidation.lireInt(constApplications.STRING_CHOIX, 1, 3);
+                    break;
+
+                case constApplications.MODE_MENU_CHOIX_NOTE:
+                    
+
+                case constApplications.MODE_QUITTER:
+                    System.out.println("Programme quitté normalement");
+                    fin = true;
+            }
+        }while(!fin);
+    
+    }
+    
+ }
