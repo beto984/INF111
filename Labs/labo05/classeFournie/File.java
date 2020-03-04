ce7f3feb3ba41f6d1ba1818f288037d82a3a5d14
Humberto <beto984@gmail.com>
2020-03-04 11:37:05 -0500

    Ajout du labo05 version 2

--- Labs/labo05/classeFournie/File.java
+++ Labs/labo05/classeFournie/File.java
@@ -1,83 +1,58 @@
 import java.util.NoSuchElementException;
-
 /**
  * File
  */
 public class File {
 
-    //------------------------------------
-    // CONSTANTES DE CLASSE
-    //------------------------------------
+    private final static int CAPACITE_INITIALE = 10;
 
-    //Capacite initiale par defaut
-    private static final int CAPACITE_INITIALE = 10;
+    private final static int INCREMENT = 10;
 
-    //le nombres de places a ajouter lorque la capacite de cette file est 
-    //atteinte (file pleine).
-    private static final int INCREMENT = 10;
-   
-    //------------------------------------
-    // ATTRIBUTS D'INSTANCE
-    //------------------------------------
-    
+
+    //ATTRIBUTS D'INSTANCE
     private int nbrElements;
     private int capacite;
-    private Object[] elements;
+    private Object[] elements; 
     private int debut;
-    private int fin;
+    private int fin; 
 
-    //------------------------------------
-    // CONSTRUCTEURS
-    //------------------------------------
-
-    public File(int capacite){
+    public File(int capaciteInit){
         nbrElements = 0;
-        this.capacite = capacite;
-        elements = new Object[capacite];
+        capacite = capaciteInit;
+        elements = new Object[capaciteInit];
         debut = 0;
-        fin = capacite - 1;
+        fin = capacite - 1; 
     }
 
     public File(){
         this(CAPACITE_INITIALE);
-    }
-
-    //------------------------------------
-    // METHODES
-    //------------------------------------
+    }    
 
     public int getNbrElements(){
         return nbrElements;
     }
 
     public boolean estVide(){
-        return nbrElements == 0;
+        return nbrElements == 0; 
     }
 
     public Object consulterDebut(){
-        Object retour = null;
+        Object retour;
+
         if(estVide()){
             throw new NoSuchElementException();
-        }else{
-            retour = elements[debut];
         }
-        return retour; 
+
+        return elements[debut]; 
     }
 
     public void enfiler(Object elem){
-       //Si la file est pleine, on augmente sa capacite de INCREMENT.
-      if (nbrElements == capacite) {
-        reallouer();
-     }
-
-     //ajuster la fin qui devient l'indice auquel enfiler l'element
-     fin = indiceSuivant(fin);
-
-     //ajouter elem a la fin
-     elements[fin] = elem;
-
-     //incrementer le nombre d'elements
-     nbrElements++;
+        if(capacite == nbrElements){
+            reallouer();
+        }
+        fin = indiceSuivant(fin);
+        elements[fin] = elem;
+        nbrElements++; 
     }
 
     public Object defiler(){
@@ -85,51 +60,39 @@ public class File {
             throw new NoSuchElementException();
         }
 
-        Object elem = consulterDebut();
+        Object retour = consulterDebut();
 
         debut = indiceSuivant(debut);
-
-        nbrElements--;
-        return elem;
+        nbrElements--; 
+        
+        return retour; 
     }
 
     public void vider(){
-        debut = 0;
         nbrElements = 0;
-        fin = capacite - 1; 
-
+        debut = 0; 
+        fin = capacite - 1 ; 
     }
 
-    //---------------------------------------------------
-   // METHODES D'INSTANCE PRIVEES (methodes utilitaires)
-   //---------------------------------------------------
 
-   private void reallouer() {
-    //Creer un nouveau tableau avec INCREMENT cases de plus
-    Object[] temp = new Object[capacite + INCREMENT]; 
-    
-    //copier les elements du tableau elements au debut de temp
-    for (int i = 0; i < nbrElements; i++) { // nbrElements elements a copier
-       temp[i] = elements[debut];
-       debut = indiceSuivant(debut); //passer a l'indice suivant dans elements
+
+
+
+    private void reallouer(){
+        Object[] temp = new Object[capacite + INCREMENT];
+
+        for(int i = 0 ; i < nbrElements; i++ ){
+            temp[i] = elements[debut];
+            debut = indiceSuivant(debut); 
+        }
+
+        elements = temp;
+        debut = 0;
+        fin = nbrElements - 1; 
+        capacite = temp.length;
     }
-    
-    //Assigner la reference de temp a elements
-    elements = temp;
-    
-    //reajuster debut, fin, et capacite
-    debut = 0;
-    fin = nbrElements - 1;
-    capacite = temp.length;
-   
- }
 
- /**
-  * Retourne l'indice qui vient apres i, dans le tableau circulaire.
-  * @param i l'indice dont on veut le suivant
-  * @return l'indice qui vient apres i, dans le tableau circulaire
-  */
- private int indiceSuivant(int i) {
-    return (i + 1) % capacite;
- }
+    private int indiceSuivant(int i) {
+        return (i + 1) % capacite;
+     }
 }
